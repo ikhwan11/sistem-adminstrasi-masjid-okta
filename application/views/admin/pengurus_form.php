17328aff0308f20143b81f2e6ee852b35002188f
ikhwan11 <ikhwansulestra13@gmail.com>
2021-05-05 15:48:35 +0700

    mendesain form tambah pengurus dan tabel

--- application/views/admin/pengurus_form.php
+++ application/views/admin/pengurus_form.php
@@ -3,60 +3,145 @@
         <div class="header-body">
             <div class="row align-items-center py-4">
                 <div class="col-lg-6 col-7">
-                    <h6 class="h2 text-white d-inline-block mb-0"><i class="fas fa-home"></i> From tambah pengurus</h6>
+                    <h6 class="h2 text-white d-inline-block mb-0"><i class="fas fa-home"></i> From tambah pengurus masjid Asy-syuhada</h6>
                 </div>
             </div>
             <div>
                 <form action="<?= base_url(''); ?>" method="POST">
+                    <h2 class="text-white"><u>Informasi diri :</u></h2>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">Nama Lengkap :</label>
+                                <label>Nama Lengkap :</label>
                                 <input type="text" class="form-control" id="nama" name="nama" autocomplete="off" value="<?= set_value('nama'); ?>" autofocus>
                                 <?php echo form_error('nama', '<span class=" text-small text-danger">', '</span>') ?>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">Jenis Kelamin :</label>
-                                <select type="text" class="form-control" id="jk" name="jk">
+                                <label>Nama Panggilan :</label>
+                                <input type="text" class="form-control" id="nama_s" name="nama_s" autocomplete="off" value="<?= set_value('nama_s'); ?>" autofocus>
+                                <?php echo form_error('nama_s', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group text-white">
+                                <label>Jenis Kelamin :</label>
+                                <select type="text" class="form-control" id="jk1" name="jk1">
                                     <option value="">== Pilih jenis kelamin ==</option>
                                     <option value="Laki-laki">Laki-laki</option>
                                     <option value="Perempuan">Perempuan</option>
                                 </select>
-                                <?php echo form_error('jk', '<span class=" text-small text-danger">', '</span>') ?>
+                                <?php echo form_error('jk1', '<span class=" text-small text-danger">', '</span>') ?>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">Tanggal lahir :</label>
+                                <label>Tanggal lahir :</label>
                                 <input type="date" class="form-control" id="tl" name="tl">
                                 <?php echo form_error('tl', '<span class=" text-small text-danger">', '</span>') ?>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">No Hp :</label>
+                                <label>No Hp :</label>
                                 <input type="number" class="form-control" id="no_hp" name="no_hp" autocomplete="off" value="<?= set_value('no_hp'); ?>">
                                 <?php echo form_error('no_hp', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group text-white">
+                                <label>Email :</label>
+                                <input type="email" class="form-control" id="email" name="email" autocomplete="off" value="<?= set_value('email'); ?>">
+                                <?php echo form_error('email', '<span class=" text-small text-danger">', '</span>') ?>
+                                <span><small class="text-muted text-yellow"> * isi jika ada</small></span>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">Jabatan :</label>
-                                <input type="text" class="form-control" id="jabatan" name="jabatan">
+                                <label>Jabatan :</label>
+                                <input type="text" class="form-control" id="jabatan" name="jabatan" value="<?= set_value('jabatan'); ?>">
                                 <?php echo form_error('jabatan', '<span class=" text-small text-danger">', '</span>') ?>
                             </div>
                         </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-6">
                             <div class="form-group text-white">
-                                <label for="tipe_transaksi">Alamat :</label>
-                                <textarea type="text" class="form-control" id="alamat" name="alamat" rows="3"><?= set_value('alamat'); ?></textarea>
-                                <?php echo form_error('alamat', '<span class=" text-small text-danger">', '</span>') ?>
+                                <label>Alamat domisili:</label>
+                                <textarea type="text" class="form-control" id="alamat1" name="alamat1" rows="3"><?= set_value('alamat1'); ?></textarea>
+                                <?php echo form_error('alamat1', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group text-white">
+                                <label>Alamat asal:</label>
+                                <textarea type="text" class="form-control" id="alamat2" name="alamat2" rows="3"><?= set_value('alamat2'); ?></textarea>
+                                <?php echo form_error('alamat2', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <!-- keluarga -->
+                    <h2 class="text-white"><u>Informasi data keluarga :</u></h2>
+                    <p class="text-yellow">silakan isi jika ada</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group text-white">
+                                <label>Nama lengkap :</label>
+                                <input type="text" class="form-control" id="pasangan" name="pasangan" value="<?= set_value('pasangan'); ?>">
+                                <?php echo form_error('pasangan', '<span class=" text-small text-danger">', '</span>') ?>
+                                <span><small class="text-muted text-yellow"> * Nama lengkap pasangan</small></span>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group text-white">
+                                <label>Hubungan :</label>
+                                <select type="text" class="form-control" id="hub" name="hub">
+                                    <option value="">== Pilih Hubungan ==</option>
+                                    <option value="Suami">Suami</option>
+                                    <option value="Istri">Istri</option>
+                                </select>
+                                <?php echo form_error('hub', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group text-white">
+                                <label>Anak Pertama :</label>
+                                <input type="text" class="form-control" id="pasangan" name="pasangan" value="<?= set_value('pasangan'); ?>">
+                                <?php echo form_error('pasangan', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group text-white">
+                                <label>Jenis Kelamin :</label>
+                                <select type="text" class="form-control" id="jk2" name="jk2">
+                                    <option value="">== Pilih jenis kelamin ==</option>
+                                    <option value="Laki-laki">Laki-laki</option>
+                                    <option value="Perempuan">Perempuan</option>
+                                </select>
+                                <?php echo form_error('jk2', '<span class=" text-small text-danger">', '</span>') ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group text-white">
+                                <label>No yang bisa dihubungi :</label>
+                                <input type="number" class="form-control" id="no_hub" name="no_hub" autocomplete="off" value="<?= set_value('no_hub'); ?>">
+                                <?php echo form_error('no_hub', '<span class=" text-small text-danger">', '</span>') ?>
                             </div>
                         </div>
                     </div>
